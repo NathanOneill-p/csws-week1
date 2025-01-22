711ed2f01a9384abb0b0b42a1a9d0b6198ccd4a9
Nathan O'Neill <oneillnatahan926@gmail.com>
2025-01-22 10:37:35 +0000

    list

--- week1.py
+++ week1.py
@@ -1,4 +1,3 @@
-language = " python "
-language.rstrip()
-language.lstrip()
-language.strip()
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[0])
+print(bicycles[0].title())
